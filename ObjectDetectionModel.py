c0b42516e35c6e91ba34fce802eae23df5d14de4
zyle-geralde <zylgerdlp19@gmail.com>
2024-09-26 17:06:43 +0800

    added new sample

--- ObjectDetectionModel.py
+++ ObjectDetectionModel.py
@@ -3,8 +3,8 @@ import cv2
 
 
 #YOLO pretrained model with COCO dataset
-model = YOLO('yolov8n.pt') #download yolo pretrained model you can change the last part to l or something else
+model = YOLO('YoloWeights/yolov8l.pt') #download yolo pretrained model you can change the last part to l or something else
 
-results = model("./sampleImage/sample1.jpg",show = True)#locate the image and show it
+results = model("./sampleImage/sample2.jpg",show = True)#locate the image and show it
 cv2.waitKey(0)#exits when the user inputs(click the x button etc.) This is needed as the image will automaticaly close if this is not applied
 
